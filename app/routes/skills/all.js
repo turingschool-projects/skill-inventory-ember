4539b1f6f84c07f6edd052ef172eb69e78da3d26
Ian Andersen <ianderse@mac.com>
2015-01-14 11:22:17 -0700

    basic ember data for skill

--- app/routes/skills/all.js
+++ app/routes/skills/all.js
@@ -10,3 +10,11 @@ export default Ember.Route.extend({
     ];
   }
 });
+
+// import Ember from 'ember';
+
+// export default Ember.Route.extend({
+//   model: function () {
+//     return this.store.find('skill');
+//   }
+// });
--- app/routes/skills/featured.js
+++ app/routes/skills/featured.js
@@ -8,3 +8,11 @@ export default Ember.Route.extend({
     ];
   }
 });
+
+// import Ember from 'ember';
+
+// export default Ember.Route.extend({
+//   model: function () {
+//     return this.store.find('note');
+//   }
+// });
