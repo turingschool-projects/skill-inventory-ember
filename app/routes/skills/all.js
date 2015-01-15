d5e6ad6cb9ea77b87d8d1094d5808cdf57f652b1
Ian Andersen <ianderse@mac.com>
2015-01-15 10:41:39 -0700

    Ember and Rails are BFF's

--- app/routes/skills/all.js
+++ app/routes/skills/all.js
@@ -1,20 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return [
-      { title: 'Be sort of cool' },
-      { title: 'Be more awesome' },
-      { title: 'Be less awesome' },
-      { title: 'Be like Steve' }
-    ];
+  model: function () {
+    return this.store.find('skill');
   }
 });
-
-// import Ember from 'ember';
-
-// export default Ember.Route.extend({
-//   model: function () {
-//     return this.store.find('skill');
-//   }
-// });
--- app/routes/skills/featured.js
+++ app/routes/skills/featured.js
@@ -1,18 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-    return [
-      { title: 'Be awesome' },
-      { title: 'Be more awesome' }
-    ];
+  model: function () {
+    return this.store.find('skill', { featured: true });
   }
 });
-
-// import Ember from 'ember';
-
-// export default Ember.Route.extend({
-//   model: function () {
-//     return this.store.find('note');
-//   }
-// });
