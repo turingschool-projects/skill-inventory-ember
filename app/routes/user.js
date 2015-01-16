d4f16fdccbb16ac43476387a384e11cdd6522ad2
Ian Andersen <ianderse@mac.com>
2015-01-16 08:30:58 -0700

    remove super userful comments

--- app/routes/user.js
+++ app/routes/user.js
@@ -5,13 +5,3 @@ export default Ember.Route.extend({
     return this.store.find('user', params.user_id);
   }
 });
-
-// import Ember from 'ember';
-
-// export default Ember.Route.extend({
-//   model: function() {
-//     return [
-//       { name: 'User 1', id: 1 }
-//     ];
-//   }
-// });
