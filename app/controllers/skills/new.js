828ff46eafbd78a2bbf92437b559cd8441d087b0
Ian Andersen <ianderse@mac.com>
2015-01-19 15:44:12 -0700

    add featured select box to new skill form

--- app/controllers/skills/new.js
+++ app/controllers/skills/new.js
@@ -3,19 +3,16 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   // replace this with data from rails api call (get all groups)
   groups: ["1407", "1409", "1410", "1412"],
+  featured: ['True', 'False'],
 
   actions: {
     saveSkill: function () {
-      alert('Save Skill!');
+      this.store.createRecord('skill', {
+        name: this.get('name'),
+        group: this.get('group')
+      }).save().then(function (skill) {
+        this.transitionToRoute('skill', skill);
+      }.bind(this));
     }
-
-    // saveSkill: function () {
-    //   this.store.createRecord('skill', {
-    //     name: this.get('name'),
-    //     group: this.get('group')
-    //   }).save().then(function (skill) {
-    //     this.transitionToRoute('skill', skill);
-    //   }.bind(this));
-    // }
   }
 });
