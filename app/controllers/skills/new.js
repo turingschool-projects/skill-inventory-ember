89ad5ebdfcba3118d4b607f804cf3d33d29bd943
Ian Andersen <ianderse@mac.com>
2015-01-19 16:06:38 -0700

    add path transition logging for debugging purposes/transition to skills index on skill save

--- app/controllers/skills/new.js
+++ app/controllers/skills/new.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  needs: ['skills'],
   // replace this with data from rails api call (get all groups)
   groups: ["1407", "1409", "1410", "1412"],
   featured: ['True', 'False'],
@@ -9,9 +10,10 @@ export default Ember.ObjectController.extend({
     saveSkill: function () {
       this.store.createRecord('skill', {
         name: this.get('name'),
-        group: this.get('group')
-      }).save().then(function (skill) {
-        this.transitionToRoute('skill', skill);
+        group: this.get('group'),
+        featured: this.get('featured'),
+      }).save().then(function () {
+        this.transitionToRoute('skills.index');
       }.bind(this));
     }
   }
