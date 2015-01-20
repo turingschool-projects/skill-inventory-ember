5853921f9b8e876b5d42a8bb740ad54fa8fd70f1
Ian Andersen <ianderse@mac.com>
2015-01-20 13:40:13 -0700

    create skill now functions properly

--- app/controllers/skills/new.js
+++ app/controllers/skills/new.js
@@ -1,19 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
-  needs: ['skills'],
   // replace this with data from rails api call (get all groups)
   groups: ["1407", "1409", "1410", "1412"],
-  featured: ['True', 'False'],
 
   actions: {
-    saveSkill: function () {
-      this.store.createRecord('skill', {
-        name: this.get('name'),
-        group: this.get('group'),
-        featured: this.get('featured'),
-      }).save().then(function () {
-        this.transitionToRoute('skills.index');
+    saveSkill: function() {
+      this.get('model').save().then(function() {
+        this.transitionToRoute('skills');
       }.bind(this));
     }
   }
--- app/models/skill.js
+++ app/models/skill.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
+  group: DS.attr('string'),
   featured: DS.attr('boolean')
 });
