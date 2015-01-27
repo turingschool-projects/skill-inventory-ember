ba012210f31b41df88cf1cf11365d5c518a39e93
Ian Andersen <ianderse@mac.com>
2015-01-27 11:02:57 -0700

    change section_id to section on skills model

--- app/models/skill.js
+++ app/models/skill.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  section_id: DS.belongsTo('section', {async: true}),
+  section: DS.belongsTo('section', {async: true}),
   featured: DS.attr('boolean'),
   // rating: DS.hasOne('rating', {async: true})
 });
