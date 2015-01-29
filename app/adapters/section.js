227e6309aa360f15784e6669d793bc0f390a25be
Ian Andersen <ianderse@mac.com>
2015-01-29 11:35:58 -0700

    divshot stuff

--- app/adapters/section.js
+++ app/adapters/section.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  // host: 'http://skill-inventory.herokuapp.com',
+  host: 'http://skill-inventory.herokuapp.com',
   namespace: 'api/v1'
 });
