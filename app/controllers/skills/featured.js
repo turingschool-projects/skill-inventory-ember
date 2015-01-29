84e06d0c275a23196330d4ef79d8390db2c1c62e
Ian Andersen <ianderse@mac.com>
2015-01-29 09:45:09 -0700

    sortBy name/module/rating

--- app/controllers/skills/featured.js
+++ app/controllers/skills/featured.js
@@ -2,4 +2,10 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   sortProperties: ['section.name'],
+
+  actions: {
+    sortBy: function (properties) {
+      this.set('sortProperties', [properties])
+    }
+  }
 });
