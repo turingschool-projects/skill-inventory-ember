4cc11ba0bb3874c4566187f14b4729924508239b
Chandra Carney <chandracarney@me.com>
2015-03-11 15:37:32 -0600

    Hound

--- app/torii-adapters/application.js
+++ app/torii-adapters/application.js
@@ -1,5 +1,5 @@
-import Ember from 'ember';
-import config from '../config/environment';
+import Ember from "ember";
+import config from "../config/environment";
 
 export default Ember.Object.extend({
   fetch: function() {
