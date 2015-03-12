b32faf4f43705bd18db34a7cd7590e7d86ab2e00
Chandra Carney <chandracarney@me.com>
2015-03-12 11:42:46 -0600

    Models updated to match new Rails db setup.

--- app/models/cohort.js
+++ app/models/cohort.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  skills: DS.hasMany('skill', {async: true}),
-  users: DS.hasMany('user', {async: true})
+  users: DS.hasMany('user', {async: true}),
+  skills: DS.hasMany('skill', {async: true})
 });
--- app/models/skill.js
+++ app/models/skill.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  section: DS.belongsTo('section', {async: true}),
-  featured: DS.attr('boolean'),
-  rating: DS.belongsTo('rating', {async: true})
+  cohorts: DS.hasMany('cohort', {async: true}),
+  tags: DS.hasMany('tag', {async: true}),
+  ratings: DS.hasMany('rating', {async: true})
 });
